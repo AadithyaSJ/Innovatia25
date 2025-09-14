65598be72cdc86e08cdc22f389d802e4512fc718
aadithyasuresh2725@gmail.com <your_email@example.com>
2025-09-14 17:43:47 +0530

    gbb brochure

--- src/pages/EventBrochure.jsx
+++ src/pages/EventBrochure.jsx
@@ -7,7 +7,7 @@ import Header from "../components/Headers";
 const events = [
   { key: "paper-powerhouse", name: "Paper Powerhouse", brochure: "/events/PAPER-POWERHOUSE-BROCHURE.png" },
   { key: "past-to-profit", name: "Past TO Profit", brochure: "/events/past-to-profit-brochure.png" },
-  { key: "golden-blueprint", name: "The Golden Blueprint", brochure: "/events/golden-blueprint-brochure.png" },
+  { key: "golden-blueprint", name: "The Golden Blueprint", brochure: "/events/The-Golden-Blueprint-Brochure.jpg" },
   { key: "cybernoir", name: "CYBERNOIR", brochure: "/events/CYBERNOIR-BROCHURE.png" },
   { key: "retro-code-chronicles", name: "Retro Code Chronicles", brochure: "/events/retro-code-chronicles-brochure.jpg" },
   { key: "vintage-webverse", name: "Vintage Webverse", brochure: "/events/vintage-webverse-brochure.png" },
